62f01083d86e919bd2d1e75f123c808b3a46b1d1
ks1590 <69412467+ks1590@users.noreply.github.com>
2024-12-30 00:18:52 +0900

    feat: 新しいInput、Checkbox、Tableコンポーネントの追加と依存関係の更新

--- app/page.tsx
+++ app/page.tsx
@@ -1,17 +1,23 @@
 import { Button } from '@/components/ui/button';
 import { ChartComponent } from '@/components/ui/pieChart';
 import { BarChartComponent } from '@/components/ui/barChart';
+import DataTable from '@/components/ui/dataTable';
 
 export default function Home() {
   return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen  p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <main className='flex gap-8 row-start-2 justify-center items-center w-full bg-main p-16 border-2 border-border shadow-shadow rounded-xl'>
+    <div className='grid grid-cols-1 gap-8 font-[family-name:var(--font-geist-sans)] max-w-7xl mx-auto justify-center'>
+      <main className='grid grid-cols-3 gap-8 justify-center items-center w-full bg-main p-16 border-2 border-border shadow-shadow rounded-xl'>
         <ChartComponent />
         <ChartComponent />
         <ChartComponent />
       </main>
-      <main className='flex gap-8 row-start-3 justify-center items-center w-full bg-main p-16 border-2 border-border shadow-shadow rounded-xl'>
-        <BarChartComponent />
+      <main className='grid grid-cols-2 gap-8 justify-center items-center w-full '>
+        <div className='grow bg-main p-10 border-2 border-border shadow-shadow rounded-xl'>
+          <BarChartComponent />
+        </div>
+        <div className='grow bg-main p-10 border-2 border-border shadow-shadow rounded-xl'>
+          <DataTable />
+        </div>
       </main>
       <Button>Default</Button>
       <footer className='row-start-3 flex gap-6 flex-wrap items-center justify-center'></footer>
