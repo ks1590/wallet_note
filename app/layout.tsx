2fa6684c4fefdc17e1f3890366eca5a5c222052f
ks1590 <69412467+ks1590@users.noreply.github.com>
2024-12-29 23:01:40 +0900

    feat: グローバルスタイルの更新、背景グリッドの追加、メインコンポーネントのスタイル調整

--- app/layout.tsx
+++ app/layout.tsx
@@ -25,7 +25,7 @@ export default function RootLayout({
   return (
     <html lang='ja'>
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}>
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-grid`}>
         {children}
       </body>
     </html>
--- app/page.tsx
+++ app/page.tsx
@@ -3,8 +3,8 @@ import { ChartComponent } from '@/components/ui/pieChart';
 
 export default function Home() {
   return (
-    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
-      <main className='flex gap-8 row-start-2 justify-center items-center w-full'>
+    <div className='grid grid-rows-[20px_1fr_20px] items-center justify-items-center min-h-screen  p-8 pb-20 gap-16 sm:p-20 font-[family-name:var(--font-geist-sans)]'>
+      <main className='flex gap-8 row-start-2 justify-center items-center w-full bg-main p-16 border-2 border-border shadow-shadow rounded-xl'>
         <ChartComponent />
         <ChartComponent />
         <ChartComponent />
