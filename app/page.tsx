654b4b680eb2984a9883bde34107b987bc6cddf7
ks1590 <69412467+ks1590@users.noreply.github.com>
2025-01-02 14:11:18 +0900

    feat: ページレイアウトの改善、セクションタグへの変更、ヘッダーにボタンを追加

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,3 @@
-import { Button } from '@/components/ui/button';
 import { ChartComponent } from '@/components/ui/pieChart';
 import { BarChartComponent } from '@/components/ui/barChart';
 import DataTable from '@/components/ui/dataTable';
@@ -10,25 +9,24 @@ export default function Home() {
     <div>
       <Header />
       <div className='grid grid-cols-1 gap-8 font-[family-name:var(--font-geist-sans)] max-w-7xl mx-auto my-8 justify-center'>
-        <main className='grid grid-cols-3 gap-8 justify-center items-center w-full bg-main p-8 border-2 border-border shadow-shadow rounded-xl'>
+        <section className='grid grid-cols-3 gap-8 justify-center items-center w-full bg-main p-8 border-2 border-border shadow-shadow rounded-xl'>
           <ChartComponent />
           <ChartComponent />
           <ChartComponent />
-        </main>
-        <main className='grid grid-cols-2 gap-8 justify-center items-center w-full '>
+        </section>
+        <section className='grid grid-cols-2 gap-8 justify-center items-center w-full'>
           <div className='grow bg-secondaryPurple p-8 border-2 border-border shadow-shadow rounded-xl'>
             <BarChartComponent />
           </div>
           <div className='grow bg-secondaryGreen p-8 border-2 border-border shadow-shadow rounded-xl'>
             <DataTable />
           </div>
-        </main>
-        <main className='grid grid-cols-1 gap-8 justify-center items-center w-full '>
+        </section>
+        <section className='grid grid-cols-1 gap-8 justify-center items-center w-full'>
           <div className='grow bg-secondaryPurple p-8 border-2 border-border shadow-shadow rounded-xl'>
             <AreaChartComponent />
           </div>
-        </main>
-        <Button>Default</Button>
+        </section>
         <footer className='row-start-3 flex gap-6 flex-wrap items-center justify-center'></footer>
       </div>
     </div>
--- components/header.tsx
+++ components/header.tsx
@@ -1,10 +1,14 @@
 import React from 'react';
+import { Button } from './ui/button';
 
 const Header = () => {
   return (
-    <header className='w-full p-4  border-b-4 border-border bg-primary'>
-      <div className='container mx-auto'>
+    <header className='w-full p-4 border-b-4 border-border bg-primary'>
+      <div className='flex justify-between max-w-7xl mx-auto'>
         <h1 className='text-2xl font-bold'>Wallet Note</h1>
+        <Button variant='neutral' className='font-bold'>
+          新規作成
+        </Button>
       </div>
     </header>
   );
